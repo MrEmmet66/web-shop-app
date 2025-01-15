7ff99127909b2ff0e19ce770945808022ac19ec0
Oleh Hlushko <lorrenc7@gmail.com>
2025-01-15 17:27:26 +0200

    change strategy behaviour to cookie-based token

--- web-shop-backend/src/auth/strategies/refreshToken.strategy.ts
+++ web-shop-backend/src/auth/strategies/refreshToken.strategy.ts
@@ -7,14 +7,20 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
     constructor() {
         super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            jwtFromRequest: ExtractJwt.fromExtractors([(req: Request) => {
+                let token = null;
+                if (req && req.cookies) {
+                    token = req.cookies['refreshToken'];
+                }
+                return token;
+            }]),
             secretOrKey: process.env.JWT_REFRESH_SECRET,
             passReqToCallback: true,
         })
     }
 
     validate(req: Request, payload: any) {
-        const refreshToken = req.get('Authorization').replace('Bearer', '').trim();
+        const refreshToken = req.cookies['refreshToken'];
         return { ...payload, refreshToken };
     }
 
