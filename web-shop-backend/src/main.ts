f12224ee1aa86018844070df3782bc9a0d196add
Oleh Hlushko <lorrenc7@gmail.com>
2025-01-15 17:27:54 +0200

    cors & cookie setup

--- web-shop-backend/src/main.ts
+++ web-shop-backend/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: false});
 
+  app.setGlobalPrefix('api/v1')
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+  })
+
+  app.use(cookieParser())
+  
   const config = new DocumentBuilder()
   .setTitle('Web Shop API')
   .setDescription('API for a web shop')
